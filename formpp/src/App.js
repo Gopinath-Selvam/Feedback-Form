529dfb820dcfec5e0a7349ed05c2550d6b9d2ab1
Gopinath Selvam <gopinathselvam16@gmail.com>
2021-11-05 19:27:36 +0530

    Feedback form updated

--- formpp/src/App.js
+++ formpp/src/App.js
@@ -5,11 +5,18 @@ import './App.css';
 
 function App() {
 
-  const [error, setError] = useState("");
+  const [averageClarity, setAverageClarity] = useState(0);
+  const [averageKnowledge, setAverageKnowledge] = useState(0);
+  const [averageTopic, setAverageTopic] = useState(0);
+  const [averageCorrection, setAverageCorrection] = useState(0);
+  const [averageInteraction, setAverageInteraction] = useState(0);
+  const [entries, setEntries] = useState(0);
+  const [clarityIsChecked, setClarityIsChecked] = useState(false);
+  const [knowledgeIsChecked, setKnowledgeIsChecked] = useState(false);
+  const [topicIsChecked, setTopicIsChecked] = useState(false);
+  const [correctionIsChecked, setCorrectionIsChecked] = useState(false);
+  const [interactionIsChecked, setInteractionIsChecked] = useState(false);
 
-  useEffect(() => {
-    setError("")
-  }, [])
 
   const validateForm = (event) => {
     event.preventDefault();
@@ -18,17 +25,66 @@ function App() {
     const feedback = document.getElementById("Feedback").value;
     const contentMaterial = document.getElementById("Material").value;
     const followups = document.getElementById("FollowUps").value;
-    if(subject == "" ||
-    staff == "" ||
-    feedback == "" ||
-    contentMaterial == "" ||
-    followups == "")
-      setError("Enter Details Correctly");
+    const clarity = document.getElementById("clarity").value;
+    const knowledge = document.getElementById("SubjK").value;
+    const topic = document.getElementById("Topic").value;
+    const correction = document.getElementById("Interaction").value;
+    const interaction = document.getElementById("Correction").value;
+    if (subject === "" ||
+      staff === "" ||
+      feedback === "" ||
+      contentMaterial === "" ||
+      followups === "")
+      alert("Enter Details Correctly");
     else {
-      setError("");
+      if (entries === 0) {
+        setAverageClarity(((clarity / 5) * 100))
+        setAverageKnowledge(((knowledge / 5) * 100))
+        setAverageTopic(((topic / 5) * 100))
+        setAverageCorrection(((correction / 5) * 100))
+        setAverageInteraction(((interaction / 5) * 100))
+      } else {
+        setAverageClarity((parseFloat(((((averageClarity / 100 * 5) * entries)) + parseInt(clarity)) / (entries + 1)) / 5) * 100)
+        setAverageKnowledge((parseFloat(((((averageKnowledge / 100 * 5) * entries)) + parseInt(knowledge)) / (entries + 1)) / 5) * 100)
+        setAverageTopic((parseFloat(((((averageTopic / 100 * 5) * entries)) + parseInt(topic)) / (entries + 1)) / 5) * 100)
+        setAverageCorrection((parseFloat(((((averageCorrection / 100 * 5) * entries)) + parseInt(correction)) / (entries + 1)) / 5) * 100)
+        setAverageInteraction((parseFloat(((((averageInteraction / 100 * 5) * entries)) + parseInt(interaction)) / (entries + 1)) / 5) * 100)
+      }
+      setEntries(entries + 1)
+      setClarityIsChecked(false)
+      setKnowledgeIsChecked(false)
+      setTopicIsChecked(false)
+      setCorrectionIsChecked(false)
+      setInteractionIsChecked(false)
       alert("Form Validated Successfully!");
     }
   }
+
+  const validateClarity = (event) => {
+    event.preventDefault();
+    setClarityIsChecked(true);
+  }
+
+  const validateKnowledge = (event) => {
+    event.preventDefault();
+    setKnowledgeIsChecked(true);
+  }
+
+  const validateTopic = (event) => {
+    event.preventDefault();
+    setTopicIsChecked(true);
+  }
+
+  const validateCorrection = (event) => {
+    event.preventDefault();
+    setCorrectionIsChecked(true);
+  }
+
+  const validateInteraction = (event) => {
+    event.preventDefault();
+    setInteractionIsChecked(true);
+  }
+
   return (
     <div className="App">
       <div class="App-header">
@@ -58,7 +114,14 @@ function App() {
         </datalist>
         <br />
         <label for="clarity" class="form-label">Clarity in Teaching</label>
-        <input type="range" class="form-range" min="0" max="5" id="clarity" />
+        <input type="range" onChange={validateClarity} class="form-range" min="0" max="5" id="clarity" defaultValue="0" />
+        <br />
+        {
+          averageClarity !== 0 && clarityIsChecked === true ?
+            <div class="progress">
+              <div class="progress-bar" style={{ width: averageClarity.toString() + "%" }} role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Average Response</div>
+            </div> : console.log(clarityIsChecked)
+        }
         <br />
         <div class="rating">
           <label for="rating" class="form-label">Rating</label>
@@ -78,7 +141,14 @@ function App() {
         </div>
         <br />
         <label for="SubjK" class="form-label">Subject Knowledge</label>
-        <input type="range" class="form-range" min="0" max="5" id="SubjK" />
+        <input type="range" onChange={validateKnowledge} class="form-range" min="0" max="5" id="SubjK" defaultValue="0" />
+        <br />
+        {
+          averageKnowledge !== 0 && knowledgeIsChecked === true ?
+            <div class="progress">
+              <div class="progress-bar" style={{ width: averageKnowledge.toString() + "%" }} role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Average Response</div>
+            </div> : console.log(clarityIsChecked)
+        }
         <br />
         <label for="Material" class="form-label">Content Material  </label>
         <select class="form-select" aria-label="Default select example" id="Material">
@@ -98,17 +168,36 @@ function App() {
 
         <br />
         <label for="Topic" class="form-label">Topic Coverage</label>
-        <input type="range" class="form-range" min="0" max="5" id="Topic" />
+        <input type="range" onChange={validateTopic} class="form-range" min="0" max="5" id="Topic" defaultValue="0" />
 
         <br />
+        {
+          averageTopic !== 0 && topicIsChecked === true ?
+            <div class="progress">
+              <div class="progress-bar" style={{ width: averageTopic.toString() + "%" }} role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Average Response</div>
+            </div> : console.log(clarityIsChecked)
+        }
+        <br />
         <label for="Correction" class="form-label">Correction Transparency</label>
-        <input type="range" class="form-range" min="0" max="5" id="Correction" />
+        <input type="range" onChange={validateCorrection} class="form-range" min="0" max="5" id="Correction" defaultValue="0" />
+        <br />
+        {
+          averageCorrection !== 0 && correctionIsChecked === true ?
+            <div class="progress">
+              <div class="progress-bar" style={{ width: averageCorrection.toString() + "%" }} role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Average Response</div>
+            </div> : console.log(clarityIsChecked)
+        }
         <br />
 
         <label for="Interraction" class="form-label">Student Interaction</label>
-        <input type="range" class="form-range" min="0" max="5" id="Interaction" />
+        <input type="range" onChange={validateInteraction} class="form-range" min="0" max="5" id="Interaction" defaultValue="0" />
         <br />
-        <div className="errorMessage">{error}</div>
+        {
+          averageInteraction !== 0 && interactionIsChecked === true ?
+            <div class="progress">
+              <div class="progress-bar" style={{ width: averageInteraction.toString() + "%" }} role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">Average Response</div>
+            </div> : console.log(clarityIsChecked)
+        }
         <br />
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
